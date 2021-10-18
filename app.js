58bc6c78faee99bdd567205597f0432511a4488b
nabaspl <nabas@piktorlabs.com>
2021-10-18 09:40:08 +0530

    port change

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use('/safe',safeRoute);
 mongoose.connect(process.env.DB_CONNECTION,()=>{
     console.log("connected");
 })
-const port = process.env.port || 8000;
-app.listen(port);
+const port = process.env.PORT || 8000;
+app.listen(port,function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
