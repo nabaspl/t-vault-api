423ead51ce9dac46b22690403cc1b9c324793c09
nabaspl <nabas@piktorlabs.com>
2021-10-18 11:15:03 +0530

    form validation

--- models/Safe.js
+++ models/Safe.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const SafeSchema = mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        unique: true
     },
     owner:{
         type:String,
